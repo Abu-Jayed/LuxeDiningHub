cc919ffae2d1d7e5d4031d29d252fef78524fc7f
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-08-15 10:01:42 +0600

    icon fixed and menu section redesign

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,33 +5,43 @@ import menu_Img_4 from "../../assets/menu/menu_4.jpg";
 import menu_Img_5 from "../../assets/menu/menu_5.jpg";
 import menu_Img_6 from "../../assets/menu/menu_6.jpg";
 import "./menu.css";
-import { BiLeaf } from "react-icons/bi";
+import { BiLeaf } from "react-icons/bi"
+import { CiPizza } from "react-icons/ci";
+import { GiHamburger, GiFishCooked,GiCannedFish } from "react-icons/gi";
+import { IoIceCreamOutline } from "react-icons/io5";
+import {CiCoffeeCup} from "react-icons/ci"
 
 const Menu = () => {
   const menuImg = [
     {
       img: menu_Img_1,
       name: "Salads",
+      icon: <BiLeaf id="icon"></BiLeaf>
     },
     {
       img: menu_Img_2,
       name: "Pizzas",
+      icon: <CiPizza id="icon"></CiPizza>
     },
     {
       img: menu_Img_3,
       name: "Burgers",
+      icon: <GiHamburger id="icon"></GiHamburger>
     },
     {
       img: menu_Img_4,
       name: "Desserts",
+      icon: <IoIceCreamOutline id="icon"></IoIceCreamOutline>
     },
     {
       img: menu_Img_5,
       name: "Drinks",
+      icon: <CiCoffeeCup id="icon"></CiCoffeeCup>
     },
     {
-      img: menu_Img_1,
+      img: menu_Img_6,
       name: "Seafood",
+      icon: <GiCannedFish id="icon"></GiCannedFish>
     },
   ];
   return (
@@ -46,19 +56,19 @@ const Menu = () => {
             <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 pointer-events-none"></div>
             <div className=" text-center flex justify-center items-center">
               <div className=" absolute top-[75px] left-[130px] text-4xl text-white">
-                <div className="rounded-full border-4 border-white border-opacity-40 p-[2px]">
+                <div className="rounded-full border-[5px] border-white border-opacity-40 p-[2px]">
                   <div
                     id="icon_bg"
                     className=" bg-[#6046b6] h-24 w-24 rounded-full  flex items-center justify-center   "
                   >
-                    <div className=" ">
-                      <BiLeaf id="icon"></BiLeaf>
+                    <div className="text-[44px] ">
+                      {data.icon}
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="absolute text-4xl text-white top-[68%] ml-[4%] " >
-                <h1 className=" cursor-pointer hover:text-[#ffe745]">
+              <div className="absolute text-white top-[68%] ml-[4%] " >
+                <h1 className=" cursor-pointer text-4xl hover:text-[#ffe745]">
                   {data.name}
                 </h1>
               </div>
